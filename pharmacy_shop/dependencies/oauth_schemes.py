5c7e415ed46aa779894c57937c48bf83bf1e913a
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-01 22:12:33 +0000

    cleaning the code

--- pharmacy_shop/dependencies/oauth_schemes.py
+++
@@ -1,3 +0,0 @@
-from fastapi.security  import OAuth2PasswordBearer
-
-user_scheme = OAuth2PasswordBearer(tokenUrl= "/users/authenticate",scheme_name="user")
--- pharmacy_shop/schema/token.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class TokenBearer(BaseModel):
-    token_type: str = "bearer"
-    token: str
