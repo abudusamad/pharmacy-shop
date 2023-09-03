494cc78b04e98163a7d8698948a306080509b55a
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-03 18:32:11 +0000

    Finished with the query part

--- examples/query.py
+++ examples/query.py
@@ -24,3 +24,13 @@ async def read_item(item_id:str, q:str|None = None, short:bool =False):
         )
         
     return item
+
+#And of course, you can define some parameters as required, some as having a default value, and some entirely optional
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+item_id:str, needy:str, skip:int=0, limit:int | None=None
+):
+    item = {
+        "item_id":item_id, "needy":needy,"skip":skip, "limit":limit
+    }
