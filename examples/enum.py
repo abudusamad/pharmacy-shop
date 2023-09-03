3f379476c1b3010e290df4d3e5b56db14615a774
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-03 17:32:42 +0000

    working at the Enum libary

--- examples/enum.py
+++ examples/enum.py
@@ -1,3 +1,16 @@
 from main import app
 
-from enum import  Enum
+from enum import Enum
+
+class ModelName(str, Enum):
+    alenet:"alenet"
+    lenet:"lenet"
+    opnet:"lenet"
+    
+@app.get("/model/{model_name}")
+async def get_model(model__name: ModelName):
+    if model__name is ModelName.alenet:
+        return{"model_name": model__name, 'message': "deep Learning"}
+    if model__name.value == "lenet":
+        return{"model_name":model__name, "message":"all the images"}
+    return{"model_name":model__name,"message":"Have some residuals"}
