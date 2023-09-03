4e3dfe0da5015625523c51a8bcbc372d52180619
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-03 17:32:42 +0000

    Working with an enumeration

--- examples/enum.py
+++ examples/enum.py
@@ -1,3 +1,18 @@
 from main import app
 
-from enum import  Enum
+from enum import Enum
+
+class ModelName(str, Enum):
+    lexnet:"lexnet"
+    masnet:"masnet"
+    josnet:"josnet"
+
+@app.get("/models/{model_name}")
+
+async def get_model(model_name:ModelName):
+    if model_name is ModelName.josnet:
+        return {"model_name":model_name, "message":"Hellow josnet how are you"}
+    if model_name.value == "lexnet":
+        return {"model_name":model_name, "message":"printing the name of the lexnet"}
+    return {"model_name": model_name, "message":"model is the same as the order one"}
+
