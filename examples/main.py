c5f72e42de8f063db954c1c51171121c68129c6f
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-05 16:19:34 +0000

    finished witht the initial stage

--- examples/main.py
+++ examples/main.py
@@ -1,8 +1,15 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 app =FastAPI()
 
 @app.get("/")
 
-def get_inform():
-    return{"message": "Hello World"}
+def read_root():
+    return {"messages": "Hello world"}
+
+@app.get("/items/{items_id}")
+
+def read_items(items_id: int, q:Union[str, None]= None):
+    return {"items": {"items_id": items_id, "q": q}}
