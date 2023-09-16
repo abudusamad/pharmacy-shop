c3b1b2e59b0b970984d0502e11518304f508b0bd
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-16 01:24:24 +0000

    working with python type

---
+++ examples/python_type.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def get_full_name(first_name:str, last_name:str):
+    full_name = first_name.title() + " " + last_name.upper()
+    return full_name
+print(get_full_name("richard ", "fordjour"))
+
+def get_name_with_age(name:str, age:int):
+    name_with_age = name + "is this old " + age
+    return name_with_age
+
+
+def process_item(items:list[str]):
+    for item in items:
+        print(item)
+        
+        
+def process_item(items:List[str]):
+    for item in items:
+        print(item)
