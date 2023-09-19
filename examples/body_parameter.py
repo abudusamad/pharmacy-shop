b66ff1183af768dcfc7cc6b64a94d04c7c6b709f
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-19 08:22:37 +0000

    Finished with the body parameter of the fastapi

--- examples/body_parameter.py
+++ examples/body_parameter.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Body
 from pydantic import BaseModel
 
 app =FastAPI()
@@ -10,6 +10,10 @@ class Item(BaseModel):
     description: str | None = None
     price : float
     tax: float | None =None
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
 
 @app.put("/items/{item_id}")
 async def update_item(
@@ -22,4 +26,10 @@ async def update_item(
     if item:
          results.update({"item":item})
     return results
-    
+
+@app.put("/items/{item_id}")
+async def update_item(item_id:int, item:Item, user:User, importance:Annotated[int, Body(gt=0)], q:str | None = None):
+    results = {"user_id":item_id, "item":item, "user":user,  "importance": importance}
+    if q:
+        results.update({"q":q})
+    return results
