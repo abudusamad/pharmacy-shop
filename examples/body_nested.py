c57c7ce93bda4ce806955837a2d3fe67146362c7
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-19 19:43:28 +0000

    Finished with the body listed

--- examples/body_nested.py
+++ examples/body_nested.py
@@ -1,17 +1,35 @@
 from typing import Union, List
-from pydantic import BaseModel
+
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
+
+class Image(BaseModel):
+    name: str
+    url: HttpUrl
+
 
 class Item(BaseModel):
     name: str
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tag: List[str] = []
+    tag: set[str] = set()
+    img: List[Image] | None = None
     
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: List[Item]
+
+
 @app.put("/items{item_id}")
 async def update_item(item_id: int, item: Item):
-    results = {"item_id":item_id, "item": item}
+    results = {"item_id": item_id, "item": item}
     return results
+
+@app.post('/offers/')
+async def post_offer(offer: Offer):
+    return offer
--- examples/body_parameter.py
+++ examples/body_parameter.py
@@ -3,12 +3,12 @@ from typing import Annotated
 from fastapi import FastAPI, Path, Body
 from pydantic import BaseModel
 
-app =FastAPI()
+app = FastAPI()
 
 class Item(BaseModel):
     name: str
     description: str | None = None
-    price : float
+    price: float
     tax: float | None =None
 
 class User(BaseModel):
@@ -17,19 +17,19 @@ class User(BaseModel):
 
 @app.put("/items/{item_id}")
 async def update_item(
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0 , le=100)], q: str | None = None,
-    item: Item| None= None,
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=100)], q: str | None = None,
+    item: Item | None= None,
 ):
     results ={"item_id": item_id}
     if q:
         results.update({"q": q})
     if item:
-         results.update({"item":item})
+         results.update({"item": item})
     return results
 
 @app.put("/items/{item_id}")
-async def update_item(item_id:int, item:Item, user:User, importance:Annotated[int, Body(gt=0)], q:str | None = None):
-    results = {"user_id":item_id, "item":item, "user":user,  "importance": importance}
+async def update_item(item_id: int, item: Item, user: User, importance: Annotated[int, Body(gt=0)], q:str | None = None):
+    results = {"user_id": item_id, "item": item, "user": user,  "importance": importance}
     if q:
         results.update({"q":q})
     return results
