c2ed4a8778e21cd793b64176db66a641c0ad5b8a
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-19 18:32:04 +0000

    Finished with the Body field of the api

--- examples/body_field.py
+++ examples/body_field.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+
 from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
 
@@ -6,15 +7,11 @@ app = FastAPI()
 
 class Item(BaseModel):
     name:str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price:float = Field(
-        gt=0, description="The price must be greater than zero"
-    )
-    tax :float | None  = None
+    description:str | None =Field(default=None, title="The description of the Item", max_length=300)
+    price:float = Field(gt=0, description="The price should be greater than zero")
+    tax: float | None = None
     
-@app.put("/items/{item_id}")
-async def update_item(item_id:int, item:Annotated[Item, Body(embed=True)]):
-    results ={"item_id":item_id, "item":item}
-    return results
+    @app.put("/items/{item_id}")
+    async def update_item(item_id:int, item:Annotated[Item, Body(embed=True)]):
+        results = {"item_id": item_id, "item":item}
+        return results
