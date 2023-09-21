33dee727c7737489487526886f9329e020145531
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-21 15:52:54 +0000

    modified some files

--- examples/dependencies/intro.py
+++ examples/dependencies/intro.py
@@ -3,6 +3,15 @@ from fastapi import Depends, FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"},{"item_name": "Bars"},{"item_name": "Baz"}]
+
+class CommonQueryParams:
+    def __init__(self, q: str |None = None, skip: int = 0, limit: int =100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+        
+
 async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
     return {"q": q, "skip": skip, "limit": limit}
 
@@ -15,3 +24,12 @@ async def read_items(common: CommonsDep):
 @app.get("/users/")
 async def read_users(commons: CommonsDep):
     return commons
+
+@app.get("/items/")
+async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)]):
+    response={}
+    if commons.q:
+        response.update({"q": commons.q})
+        items = fake_items_db[commons.skip : commons.skip + commons.limit]
+        response.update({"items": items})
+        return response
