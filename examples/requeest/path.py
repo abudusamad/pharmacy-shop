9ebeb133cc65a0b9226985f57994b0a63ec26f6d
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-21 18:10:35 +0000

    Adding description to it

--- examples/requeest/path.py
+++ examples/requeest/path.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI, status
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
+
+# Tags with Enums¶
+# If you have a big application, you might end up accumulating several tags, and you would want to make sure you always use the same tag for related path operations.
+
+# In these cases, it could make sense to store the tags in an Enum.
+
+# FastAPI supports that the same way as with plain strings:
+
+class Tags(Enum):
+    items= "items"
+    users: "users"
+    
+@app.get("/items/", tags=[Tags.items])
+async def get_items():
+    return["Portal gun", "Plumbus"]
+
+@app.get("/users/", tags=[Tags.users])
+async def read_users():
+    return ["Risky", "Morty"]
 
 class Item(BaseModel):
     name: str
