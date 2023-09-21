a78550276762e859ee3c41e3fdbd3f2ae069d211
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-21 16:11:30 +0000

    working with tthe common name

---
+++ examples/dependencies/common.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+
+app= FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bens"}, {"item_name": "Bars"}]
+
+class CommonQueryParamas:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+        
+
+@app.get("/items/")
+async def read_items(commons: Annotated[CommonQueryParamas, Depends(CommonQueryParamas)]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+        items=fake_items_db[commons.ski: commons.skip + commons.limit]
+        response.update({"items": items})
+        return response
