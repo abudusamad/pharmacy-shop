5a499c3fa9681c188b682f9d56e7dd367339e63e
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-21 15:44:35 +0000

    working with the dependencies of the calling function

--- examples/dependencies/intro.py
+++ examples/dependencies/intro.py
@@ -6,10 +6,12 @@ app = FastAPI()
 async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
     return {"q": q, "skip": skip, "limit": limit}
 
+CommonsDep = Annotated[dict, Depends(common_parameters)]
+
 @app.get("/items/")
-async def read_items(common: Annotated[dict, Depends(common_parameters)]):
+async def read_items(common: CommonsDep):
     return common
 
 @app.get("/users/")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_users(commons: CommonsDep):
     return commons
