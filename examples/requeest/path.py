c0e8d4f304a5e6cd474dd3bd3559b2a273196c87
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-21 18:16:01 +0000

    making a fake database for the objects

--- examples/requeest/path.py
+++ examples/requeest/path.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 from enum import Enum
+from datetime import datetime
+from fastapi.encoders import jsonable_encoder
+
 
 app = FastAPI()
 
@@ -29,8 +32,19 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
     tags= set[str] = []
+    title: str
+    timestamp: datetime
+    description: str | None = None
+
     
 
 @app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
 async def create_item(item: Item):
     return item
+
+fake_db = {}
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
