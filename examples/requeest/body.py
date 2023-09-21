cd316fc501d43a78b180fc1f2db153d031155e06
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-21 13:37:37 +0000

    working with the body of the path

--- examples/requeest/body.py
+++ examples/requeest/body.py
@@ -25,4 +25,4 @@ async def create_item(item_id: str):
 async def read_item(item_id: str, item: Item):
     update_item_encoded = jsonable_encoder(item)
     items[item_id] = update_item_encoded
-    return update_item_encoded
+    return update_item_encoded 
