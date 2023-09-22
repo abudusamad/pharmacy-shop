19b56296d0a80099be89f3de47569bf01f88482b
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-22 00:47:02 +0000

    working with request model

--- examples/requeest/model.py
+++ examples/requeest/model.py
@@ -1,71 +1,24 @@
-from typing import List, Union, Any
+from typing import List, Union
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Response
-from fastapi.responses import RedirectResponse
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 app = FastAPI()
 
 class Item(BaseModel):
-    name:str
+    name: str
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tags: List[str] = []
-
+    tags : List[str] = []
     
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Union[str, None] = None
-    
-
-class BaseUser(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Union[str, None] = None
- 
-class UserIn(BaseUser):
-    password: str
-    
-    
-@app.post("/items/", response_model=Item)
-async def create_items(item: Item) -> Any :
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
     return item
 
-@app.post("/Users/", response_model=UserOut)
-async def create_user(user: UserIn) -> Any:
-    return user
-
-@app.post("/user/")
-async def create_user(user: UserIn) -> BaseUser:
-    return user
-
-@app.get("/items/", response_model=List[Item])
-async def read_items() -> Any:
-    return[
-        Item(name= "Portal Gun", price= 45.6),
-        Item(name= "Plumbus", price=32.0)
+@app.get("/items")
+async def read_item() -> List[Item]:
+    return [
+        Item(name= "Portal Gun", price = 43.5),
+        Item(name = " Plumbus", price = 45.4, description="The name of the product is the same"),
     ]
-    
-
-
-@app.get("/portal", response_model=None)
-async def get_portal(teleport: bool = False) -> Union[Response, dict]:
-    if teleport:
-        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    return {"message": "Here's your interdimensional portal."}
-
-
-
-
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
-
-
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: str):
-    return items[item_id]
