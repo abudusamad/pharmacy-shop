e34102a3cf9a7f1966ddd4ce7b85911cd3fa0a2e
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-22 16:30:30 +0000

    making the some modifiaction in the upload file of the request model

--- examples/requeest/status_code.py
+++ examples/requeest/status_code.py
@@ -1,5 +1,7 @@
+from typing import Annotated
+
+
 from fastapi import FastAPI, Form, File, UploadFile
-from typing_extensions import Annotated
 
 
 
@@ -14,9 +16,15 @@ async def create_item(name: str):
     return {"name": name}
 
 @app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+async def create_file(file: Annotated[bytes | None, File()] = None):
+    if not file:
+        return { "message": "No file sent"}
+    else:   
+        return {"file_size": len(file)}
 
 @app.post("/uploadfiles/")
-async def create_upload_file(file: UploadFile):
-    return{"filename": file.filename}
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:      
+        return{"filename": file.filename}
