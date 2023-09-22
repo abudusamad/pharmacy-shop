c90252e394e1fe8108f9a18fae9d1f4b54fbe559
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-22 17:42:42 +0000

    working with handle with errors

--- examples/requeest/errors.py
+++ examples/requeest/errors.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 class UnicornException(Exception):
     def __init__(self, name: str):
@@ -7,6 +10,20 @@ class UnicornException(Exception):
     
 
 app = FastAPI()
+
+@app.exception_handler(StarletteHTTPException)
+async def http__exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code= 418, detail ="Nope! I don't like 3.")
+    return {"item_id": item_id}
 
 @app.exception_handler(UnicornException)
 async def unicorn_exception_handler(request: Request, exc: UnicornException):
