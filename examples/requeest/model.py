146d464985dc577ad81bd8b9b917b1fd96973304
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-22 11:32:05 +0000

    validation the account with email and password session

--- examples/requeest/model.py
+++ examples/requeest/model.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Union
 from fastapi import FastAPI
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
@@ -12,10 +12,19 @@ class Item(BaseModel):
     tax: Union[float, None] = None
     tags : List[str] = []
     
+class UserIn(BaseModel):
+    username:str
+    password:str
+    email: EmailStr
+    full_name: str | None = None
+    
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item) -> Any:
     return item
 
+@app.post("/users")
+async def create_user(user: UserIn):
+    return user
 @app.get("/items", response_model= List[Item])
 async def read_item() -> Any:
     return [
