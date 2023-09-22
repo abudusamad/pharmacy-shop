3c289051e899cd9bfa61ccafc5ae11c02c66629c
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-22 16:16:15 +0000

    Working with files of the request

--- examples/requeest/status_code.py
+++ examples/requeest/status_code.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Fil
+from typing_extensions import Annotated
+
+
 
 app = FastAPI()
 
+@app.post("/logins/")
+async def create_login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
 
 @app.post("/items/", status_code=201)
 async def create_item(name: str):
