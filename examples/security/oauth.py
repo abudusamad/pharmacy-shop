8f371efae0e886b218cf47128d432471b2490d1a
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-24 15:55:02 +0000

    working with the security section of the api

--- examples/security/oauth.py
+++ examples/security/oauth.py
@@ -1,25 +1,25 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from pydantic import BaseModel, EmailStr
 
-fake_user_db= {
+fake_users_db= {
     "mascot":{
         "username": "mascot",
-        "full_name": "mascot ford",
-        "email":"abudusamed@gmail.com",
+        "full_name": "Mascot Ford",
+        "email": "abudusamed@gmail.com",
         "hashed_password": "fakehashedsecret",
-        "disabled": False,
+        "disabled": False
     },
-       "richard":{
+      "rich":{
         "username": "rich",
-        "full_name": "Richard Oppong",
-        "email":"richoppong@gmail.com",
+        "full_name": "Mandem Ford",
+        "email": "abudusamed@gmail.com",
         "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },   
-    
+        "disabled": True
+    },
 }
 
 app = FastAPI()
@@ -27,69 +27,67 @@ app = FastAPI()
 def fake_hash_password(password: str):
     return "fakehashed" + password
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl= "token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class User(BaseModel):
     username: str
     email: EmailStr | None = None
     full_name: str | None = None
-    disabled: bool | None =  None
-
-
-class UserInDB(User):
-    hashed_password: str
+    disabled: bool | None  = None
     
+class UserInDB(User):
+    hashed_password:str
+    
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
     
-    
 def fake_decode_token(token):
-    user = get_user(fake_user_db, token)
+    user = get_user(fake_users_db)
     return user
 
-async def get_currrent_user(token: Annotated[str, Depends(oauth2_scheme)]):
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Invalide authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
     return user
 
 async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_currrent_user)]
+    current_user:Annotated[User, Depends(get_current_user)]
 ):
     if current_user.disabled:
         raise HTTPException(
-            status_code = status.HTTP_400_BAD_REQUEST,
+            status_code= 400, 
             detail="Inactive user"
         )
     return current_user
-    
-    
+
 @app.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_user_db.get(form_data.username)
+    user_dict = fake_users_db.get(form_data.username)
     if not user_dict:
         raise HTTPException(
-            status_code= 400,
+            status_code=400,
             detail="Incorrect username or password"
         )
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
+    user =UserInDB(**user_dict)
+    hashed_password= fake_hash_password(form_data.password)
+    if not hashed_password:
         raise HTTPException(
             status_code=400, 
             detail="Incorrect username or password"
         )
-    return { "access_token": user.username, "token_type": "bearer"}
-
+    return {"access_token": user.username, "token_type": "bearer"}
 
 @app.get("/users/me")
-async def read_user_me(
+async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
-    return current_user
+    return current_user 
