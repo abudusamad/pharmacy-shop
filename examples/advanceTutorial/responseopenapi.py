3cd47758cb3e6daa369c8b473fac9acf27459830
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-09-29 23:46:21 +0000

    Working with the reponse model

--- examples/advanceTutorial/responseopenapi.py
+++ examples/advanceTutorial/responseopenapi.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, responses
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+
 
 class Item(BaseModel):
 	id: str
 	value: str
+
+
+
+responsess = {
+	404: {"description": "Item not found"},
+	302: {"description": "The item was moved"},
+	403: {"description": "Not enough privileges"},
+}
 
 
 class Message(BaseModel):
@@ -18,21 +29,10 @@ app = FastAPI()
 @app.get(
 	"/items/{item_id",
 	response_model=Item,
-	responses={
-		404:{"model": Message, "description": "The item was not found"},
-		200:{
-			"description": "Item requested by ID",
-			"content":{
-				"application/json": {
-					"example" : {"id": "bar", "value": "The bar tenders"}
-				}
-
-			}
-		}
-	}
+	responses={**responses, 200: {"content": {"image/png":()}}}
 
 )
-async def read_item(item_id: str):
+async def read_item(item_id: str, img: Union[bool, None] = None):
 	if item_id == "foo":
 		return {"id": "foo", "value": "there goes my hero"}
 	else:
