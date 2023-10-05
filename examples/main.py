7f1adf91070ae55f07475d760824fa91d717f5f9
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 20:29:47 +0000

    modified some part of the main

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 
 
 app = FastAPI()
 
-@app.get("/headers-and-object/")
-def get_headers(response: Response):
-	response.headers["X-Cat-Dog"] = "alone in the world"
-	return {"message": "Hello word"}
+@app.get("/headers/")
+def get_headers():
+	content = {"message": "Hello World"}
+	headers = {"X-Cat-Dog": "alone  in the world", "Content-Language": "en-US"}
+	return JSONResponse(content=content, headers=headers)
