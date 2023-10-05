81cc0bfc019887815bfe5a6faa280d97804fa5c9
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 20:22:06 +0000

    okay
    Co-authored-by: Mastech <82134715+abudusamad@users.noreply.github.com>

--- examples/main.py
+++ examples/main.py
@@ -1,31 +1,10 @@
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Response
+
+
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: int
-    is_active: Union[bool, None] = None
-
-
-@app.get("/")
-def read_root():
-    return {"messages": "Hello world"}
-
-
-app.get("/items/{items_id}")
-
-
-def read_items(items_id: int, q: Union[str, None] = None):
-    return {"items_id": items_id, "q": q}
-
-
-@app.put("/items/{items_id}")
-def put_items(items_id: int, items: Item):
-    return {"Items_name": items.name, "item_id": items_id}
-
-def get_inform():
-    return{"message": "Hello World"}
+@app.get("/headers-and-object/")
+def get_headers(response: Response):
+	response.headers["X-Cat-Dog"] = "alone in the world"
+	return {"message": "Hello word"}
