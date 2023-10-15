805d7ea638f5ee66cc628d2044fb459404407790
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-15 21:59:14 +0000

    Adding the get request model of the api for the model and reset

--- examples/advanceTutorial/security/OAuth2.py
+++ examples/advanceTutorial/security/OAuth2.py
@@ -152,5 +152,14 @@ async def login_for_access_token(
     )
     return {"access_token": access_token, "token_type":"bearer"}
 
+@app.get("/users/me", access_token, response_model=User)
+async def read_users_me(
+        current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app
+
 
             
